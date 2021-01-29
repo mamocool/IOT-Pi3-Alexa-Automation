06eec11772a543914aa0e2bce44f87fdc1037fc9
mamocool <78208459+mamocool@users.noreply.github.com>
2021-01-29 11:51:55 +0100

    Update RPi_name_port_gpio.py

--- RPi_name_port_gpio.py
+++ RPi_name_port_gpio.py
@@ -22,7 +22,7 @@ class device_handler(debounce_handler):
     """
     #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
     #TRIGGERS = {"Light": 52000}
-    TRIGGERS = {"LED": 52000,"FAN":51000}
+    TRIGGERS = {"TV": 52000,"TV":51000}
 
     def act(self, client_address, state, name):
         print("State", state, "from client @", client_address)
